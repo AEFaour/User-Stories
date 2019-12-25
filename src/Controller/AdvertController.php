0c0987a8010df8308ae700054f5789bdb2b94a62
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2019-12-25 15:30:03 +0100

    Require annotations & init Routes

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -5,19 +5,61 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
+
+/**
+ * @Route("/advert")
+ * Class AdvertController
+ * @package App\Controller
+ */
 class AdvertController extends AbstractController
 {
+    /**
+     * @Route("/{page}", name="advert_index", requirements={"page" = "\d+"}, defaults={"page" = 1})
+     * @return Response
+     */
+    public function index()
+    {
+        $url = $this->generateUrl(
+            "advert_view",
+            ['id' => 5]
+        );
 
-    public function index(Environment $environment)
-    {
-        $content = $environment->render("Advert/index.html.twig", ['name' => 'Ayatollah TAGUEULE']);
-        return new Response($content);
+        return new Response("L'URL de l'annonce 5 est: " . $url);
     }
-    public function close(Environment $environment)
+
+    /**
+     * @Route("/view/{id}", name="advert_view", requirements={"id" = "\d+"})
+     * @param $id
+     * @return Response
+     */
+    public function view($id)
     {
-        $content = $environment->render("Advert/close.html.twig", ['name' => 'Ayatollah TAGUEULE']);
-        return new Response($content);
+        return new Response("Affichage de l'annonce d'id: " . $id);
     }
+
+    /**
+     * @Route("/add", name="advert_add")
+     */
+    public function  add(){
+
+    }
+
+    /**
+     * @Route("/edit/{id}", name="advert_edit", requirements={"id" = "\d+"})
+     */
+    public function  edit(){
+
+    }
+
+    /**
+     * @Route("/delete/{id}", name="advert_delete", requirements={"id" = "\d+"})
+     */
+    public function  delete(){
+
+    }
+
+
 }
